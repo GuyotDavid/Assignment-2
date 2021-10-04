52828388e33c06b35660d08875a5995d4b8c702a
isabiiil <isabelabonitalla@gmail.com>
2021-10-03 20:48:02 -0400

    add myMap

--- functions.js
+++ functions.js
@@ -9,8 +9,11 @@ Array.prototype.myEach = function(callBack)
 }
 
 // MAP //
-Array.prototype.myMap = function() {
-
+Array.prototype.myMap = function(callback) {
+  arr = [];
+  for (var i = 0; i < this.length; i++)
+    arr.push(callback(this[i], i, this));
+  return arr;
 };
 
 // FILTER //
